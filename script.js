f0b1239f437927d7125373e599a1527e35b0b4a9
Kevin Quang <kquang8pd@gmail.com>
2024-08-11 19:24:31 -0400

    Created a webpage with a 16x16 grid of square divs
    - Created a div with class container told hold the squares in
    - Added styling to css so the grid would fit and appear
    - Added a loop that would append to the container and create multiple squares

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16 * 16; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.appendChild(square);
+
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "#333"
+  })
+}
