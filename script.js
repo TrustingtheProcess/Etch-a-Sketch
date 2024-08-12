5dbfbec9e24fef8c2188c80cfc3aa9c36b3f0b00
Kevin Quang <kquang8pd@gmail.com>
2024-08-11 20:32:00 -0400

    Created function that allowed the button to reset and create new grid

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#new-grid");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+function createGrid(size) {
+  container.textContent = '';
 
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "#333"
-  })
+  const squareSize = container.clientWidth / size;
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "#333"
+    });
+  } 
 }
+
+button.addEventListener('click', () => {
+  let gridSize = prompt("Enter the number of squares per side (max 100):", 16);
+
+  gridSize = Math.min(Math.max(parseInt(gridSize), 1), 100);
+
+  createGrid(gridSize);
+});
+
+createGrid(16);
